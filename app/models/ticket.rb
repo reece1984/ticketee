4cc3015a7c51ac40ea64966b4424ae93d6b937e0
Reece1984 <jamesreece@live.co.uk>
2013-01-15 23:13:30 +0000

    When updating a comments's status, it also updates the ticket's status

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
+  belongs_to :state
   belongs_to :user
+
   validates :title, :presence => true
   validates :description, :presence => true, :length => { :minimum => 10 }
 
