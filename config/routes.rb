d89932018aff1f094ab5ee3aacacbdd405e2a1bd
Reece1984 <jamesreece@live.co.uk>
2013-01-16 20:11:22 +0000

    Added Admin::StatesController for managing states

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,7 @@ Ticketee::Application.routes.draw do
     resources :users do
       resources :permissions
     end
+    resources :states
   end
   # The priority is based upon order of creation:
   # first created -> highest priority.
